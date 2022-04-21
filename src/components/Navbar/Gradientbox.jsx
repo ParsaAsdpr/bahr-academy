bb2c6dd3ba9f82988fed78dda69ca5be9564f148
Parsa <parsaasadpour999@gmail.com>
2022-04-21 21:09:59 +0430

    finished gradient box

--- src/components/Navbar/Gradientbox.jsx
+++ src/components/Navbar/Gradientbox.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const GradientBox = () => {
+const GradientBox = props => {
   return (
     <div
-      class="gradient-box bg-gradient-to-r from-cyan-200 to-violet-500 rounded-br-full 
-    h-full md:flex items-center justify-around lg:w-72 md:w-60 md:px-5 lg:px-8 hidden"
-    ></div>
+      className={`gradient-box bg-gradient-to-r from-${props.fc}-200 to-${props.sc}-500 rounded-br-full 
+    h-full md:flex items-center justify-around lg:w-72 md:w-60 md:px-5 lg:px-8 hidden`}
+    >
+      {props.children}
+    </div>
   );
 };
 
