82ce4935799b19e9c7c6f35dd1d19ad529861bd9
Parsa <parsaasadpour999@gmail.com>
2022-04-21 20:35:48 +0430

    created gradient box component for navbar

---
+++ src/components/Navbar/Gradientbox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GradientBox = () => {
+  return (
+    <div
+      class="gradient-box bg-gradient-to-r from-cyan-200 to-violet-500 rounded-br-full 
+    h-full md:flex items-center justify-around lg:w-72 md:w-60 md:px-5 lg:px-8 hidden"
+    ></div>
+  );
+};
+
+export { GradientBox };
