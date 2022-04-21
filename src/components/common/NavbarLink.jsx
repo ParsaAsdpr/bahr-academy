0284076bde56741f7288670437af3894438bd0a5
Parsa <parsaasadpour999@gmail.com>
2022-04-21 16:03:29 +0430

    created navbar link component

---
+++ src/components/common/NavbarLink.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavbarLink = (props) => {
+  return (
+    <a
+      href="#"
+      class="mx-3 lg:py-5 py-3 hover:border-b-2 border-indigo-500 hover:text-indigo-500 duration-75 md:block hidden text-sm lg:text-base"
+    >
+      {props.title}
+    </a>
+  );
+};
+
+export { NavbarLink };
