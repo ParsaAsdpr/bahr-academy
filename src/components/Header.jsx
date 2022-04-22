5628042111b4b953c326c909bbdf5acb5e01ee67
Parsa <parsaasadpour999@gmail.com>
2022-04-22 14:52:22 +0430

    created header component and imported navbar in it

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Navbar } from "./Navbar/Navbar";
+
+const Header = () => {
+  return <header className="bg-white w-full shadow-md h-16">
+      <Navbar></Navbar>
+  </header>;
+};
+
+export {Header};
