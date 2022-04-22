303dfc0bbfa1c50c3586a07a3863fa900dbae29f
Parsa <parsaasadpour999@gmail.com>
2022-04-22 14:30:55 +0430

    fixed a bug

--- src/components/Navbar/Gradientbox.jsx
+++ src/components/Navbar/Gradientbox.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const GradientBox = props => {
   return (
     <div
-      className={`gradient-box bg-gradient-to-r from-${props.fc}-200 to-${props.sc}-500 rounded-br-full 
+      className={`gradient-box bg-gradient-to-r from-cyan-200 to-violet-500 rounded-br-full 
     h-full md:flex items-center justify-around lg:w-72 md:w-60 md:px-5 lg:px-8 hidden`}
     >
       {props.children}
