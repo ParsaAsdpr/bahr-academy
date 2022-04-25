6f3cf3123e475738b2a3da9308a9a3421186ddc8
Parsa <parsaasadpour999@gmail.com>
2022-04-25 21:19:30 +0430

    added flipcard component in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './font.css'
 import { Navbar } from './components/Navbar/Navbar.jsx';
+import SignFlip from './components/Signup/SignFlip';
 
 function App() {
   return (
     <div className="bg-red-100 h-screen w-full font-irsans" dir='rtl'>
       <Navbar></Navbar>
+      <SignFlip></SignFlip>
     </div>
   );
 }
