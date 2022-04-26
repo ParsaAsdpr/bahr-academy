005b7ca8734f06c8b33225d07c621eee1c5a8355
Parsa <parsaasadpour999@gmail.com>
2022-04-26 18:09:24 +0430

    created header component

---
+++ src/components/Header/SearchBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBox = () => {
+    return ( 
+        <div className='mt-10 w-1/2 bg-cyan-200 bg-opacity-50 h-20 '>
+
+        </div>
+     );
+}
+ 
+export default SearchBox;
---
+++ src/components/Header/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../../Containers/Header.css"
+
+const HeaderTitle = () => {
+    return ( 
+        <div className='w-full'>
+            <h1 className='text-4xl text-sky-200 w-1/3 mt-36 text header-title'>خود آموزی سریع، کسب تجربه و ورود به بازار کار با هزینه کم با آکادمی ما</h1>
+            <p className='text-md text-stone-300 text-center w-2/3 mt-10 leading-loose'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون</p>
+        </div>
+     );
+}
+ 
+export default HeaderTitle;
