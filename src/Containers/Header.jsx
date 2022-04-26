0ba9da2bb5319d468129cea03edc150e022f99c5
Parsa <parsaasadpour999@gmail.com>
2022-04-26 18:08:40 +0430

    created containers folder for website parts

---
+++ src/Containers/Header.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Mask from "../components/common/mask";
+import SearchBox from "../components/Header/SearchBox";
+import HeaderTitle from "../components/Header/Title";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <header className="flex justify-center w-full h-screen header-landing relative">
+        <Mask cName="flex items-center flex-col flex-wrap whitespace-nowrap">
+          <HeaderTitle></HeaderTitle>
+          <SearchBox></SearchBox>
+        </Mask>
+      </header>
+    );
+  }
+}
+
+export { Header };
