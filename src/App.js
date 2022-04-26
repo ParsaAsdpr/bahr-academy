4b0472a60e0836c24e176976581827b2d7615232
Parsa <parsaasadpour999@gmail.com>
2022-04-26 18:13:48 +0430

    tested header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import './font.css'
 import { Navbar } from './components/Navbar/Navbar.jsx';
 import SignFlip from './components/Signup/SignFlip';
+import { Header } from './Containers/Header';
 
 function App() {
   return (
-    <div className="bg-red-100 h-screen w-full font-irsans" dir='rtl'>
+    <div dir='rtl'>
       <Navbar></Navbar>
-      <SignFlip></SignFlip>
     </div>
   );
 }
