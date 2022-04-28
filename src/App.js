e777b93167b3efb0ea219331be190ff52ef9a595
Parsa <parsaasadpour999@gmail.com>
2022-04-28 15:42:34 +0430

    finished 404 page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './font.css'
 import { Navbar } from './components/Navbar/Navbar.jsx';
 import SignFlip from './components/Signup/SignFlip';
 import { Header } from './Containers/Header';
+import NotFound from './Containers/404/404';
 
 function App() {
   return (
     <div dir='rtl'>
       <Navbar></Navbar>
+      <NotFound></NotFound>
     </div>
   );
 }
---
+++ src/Containers/404/404.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import notfoundpic from "../../assets/images/404.png";
+import PrimaryButton from '../../components/common/PrimaryButton';
+
+const NotFound = () => {
+    return ( 
+        <div className='w-full flex whitespace-nowrap flex-wrap justify-center'>
+            <div className='flex justify-center items-center mt-20'>
+                <img src={notfoundpic}/>
+            </div>
+                        <h1 className='md:text-5xl sm:text-4xl text-2xl font-bold text-gray-800 text-center w-full'>متاسفیم</h1>
+                        <p className='sm:text-lg text-sm text-slate-500 text-center w-full my-8 px-3 h-auto inline'>مثل اینکه صفحه مورد نظر شما یافت نشد</p>
+                        <PrimaryButton url="#" text="بازگشت به صفحه اصلی"></PrimaryButton>
+        </div>
+
+     );
+}
+ 
+export default NotFound;
