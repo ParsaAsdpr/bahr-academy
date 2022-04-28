10a95487c502a5e6f9cc1356a08f9095bdaddd8d
Parsa <parsaasadpour999@gmail.com>
2022-04-28 14:46:13 +0430

    created course components

---
+++ src/components/Courses/CourseCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+ class CourseCard extends Component {
+     state = {  } 
+     render() { 
+         return (
+             <div className='w-3/4 sm:w-1/2 mx-auto md:w-1/4 rounded-2xl overflow-hidden my-7 md:my-0"'></div>
+         );
+     }
+ }
+  
+ export default CourseCard;
