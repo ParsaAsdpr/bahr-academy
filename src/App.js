469a5c7a2bf44e65666682248c2bf5c5a333d7f3
Parsa <parsaasadpour999@gmail.com>
2022-04-28 17:33:08 +0430

    finished sign up page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Navbar } from './components/Navbar/Navbar.jsx';
 import SignFlip from './components/Signup/SignFlip';
 import { Header } from './Containers/Header';
 import NotFound from './Containers/404/404';
+import Signup from './Containers/Signup/Signup';
 
 function App() {
   return (
     <div dir='rtl'>
       <Navbar></Navbar>
+      <Signup></Signup>
     </div>
   );
 }
