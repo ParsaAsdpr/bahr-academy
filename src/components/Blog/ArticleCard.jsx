349ad944fbc50dc7902a67f054d71713ebde6967
Parsa <parsaasadpour999@gmail.com>
2022-04-29 21:54:51 +0430

    finished blog card

---
+++ src/components/Blog/ArticleCard.jsx
@@ -0,0 +1,18 @@
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+const ArticleCard = props => {
+    return ( 
+        <div className='p-7 rounded-lg' style={{boxShadow: "0px 0px 12px 1px #00000022"}}>
+            <img src={props.image} alt="blog picture" className='rounded-md'/>
+            <h2 className='text-md font-bold text-slate-800 mt-5 mb-3'>{props.title}</h2>
+            <p className='text-xs text-slate-600 h-36 leading-loose overflow-hidden overflow-ellipsis'>{props.desc}</p>
+            <a className='text-sm text-sky-500 mt-4 float-right hover:text-sky-600 transition-colors font-bold' href='#'>خوادن مقاله
+            <FontAwesomeIcon icon={faArrowLeft} className="mx-2"></FontAwesomeIcon>
+            </a>
+        </div>
+     );
+}
+ 
+export {ArticleCard};
