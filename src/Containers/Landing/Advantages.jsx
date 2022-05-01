e074aedc08472ffe84b33584d58447b8c29e234e
Parsa <parsaasadpour999@gmail.com>
2022-05-01 20:41:37 +0430

    fixed advantages bugs

--- src/Containers/Landing/Advantages.jsx
+++ src/Containers/Landing/Advantages.jsx
@@ -30,7 +30,7 @@ class Advantages extends Component {
     return (
       <section className="flex justify-center max-w-7xl mx-auto flex-col px-5">
         <Title text="مزایای آکادمی بحر" cName="text-center"></Title>
-        <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
+        <div className="grid gap-5 grid-cols-1 md:grid-cols-3">
           {advantages.map((advantage, index) => {
             return (
               <Advantage
