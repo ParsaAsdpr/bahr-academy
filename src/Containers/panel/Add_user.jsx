747abe85b364feafef2beb72654428cbafc72875
amin-jf <amin_4142@yahoo.com>
2022-05-09 20:01:54 +0430

    add user finished

--- src/Containers/panel/Add_user.jsx
+++ src/Containers/panel/Add_user.jsx
@@ -61,7 +61,7 @@ class Add_user extends Component {
           <div className="w-[90%] h-96 mt-16">
             <div className="flex">
               <input
-                className="border border-[#CBCBCB] w-[580px] rounded-[10px] h-9 text-[12px] indent-2 bg-[#eeeeee] text-[#cbcbcb]"
+                className="border border-[#CBCBCB] w-[580px] rounded-[10px] h-9 text-[12px] indent-2 bg-[#eeeeee] text-black active::border-none"
                 placeholder="جستجوی همه ی کاربران ..."
                 type="text"
               />
@@ -79,16 +79,16 @@ class Add_user extends Component {
                 افزودن کاربر
               </a>
             </div>
-            <table className="table w-full text-center h-[400px] mt-6 text-[14px] border border-red-800">
+            <table className="table w-full text-center h-[400px] mt-6 text-[14px]">
               <thead className="text-white text-[12px]">
-                <tr className="bg-[#2eb2a4] py-6">
-                  <th>نام کاربری</th>
+                <tr className="bg-[#2eb2a4] rounded-xl">
+                  <th className="py-3 rounded-tr-lg">نام کاربری</th>
                   <th>ایمیل</th>
                   <th>وضعیت</th>
                   <th>دسته بندی</th>
                   <th>آخرین بازدید</th>
                   <th>پروفابل</th>
-                  <th>مدیریت</th>
+                  <th className="py-3 rounded-tl-lg">مدیریت</th>
                 </tr>
               </thead>
               <tbody>
@@ -100,10 +100,15 @@ class Add_user extends Component {
                     <td>{d.category}</td>
                     <td>{d.last_visit}</td>
                     <td>
-                      <a>مشاهده پروفایل</a>
+                      <a href="#" className="underline text-blue-500">
+                        مشاهده پروفایل
+                      </a>
                     </td>
                     <td>
-                      <div></div>
+                      <div className="flex justify-between w-12 mx-auto">
+                        <a className="pen w-4 h-4 cursor-pointer"></a>
+                        <a className="bin w-4 h-4 cursor-pointer"></a>
+                      </div>
                     </td>
                   </tr>
                 ))}
