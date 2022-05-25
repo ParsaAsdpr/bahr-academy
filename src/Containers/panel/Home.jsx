36760897c6c2288f63f7383e5b4a16fe7e67659b
amin-jf <amin_4142@yahoo.com>
2022-05-25 14:14:25 +0430

    list user

--- src/Containers/panel/Home.jsx
+++ src/Containers/panel/Home.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Dashboard from "../../components/panel/Dashboard";
 import Header_panel from "../../components/panel/Header_panel";
+import { useState } from "react";
+import DatePicker, { Calendar } from "react-multi-date-picker";
 
-class Home extends Component {
-  state = {};
-  render() {
-    return (
+const Home = () => {
+  const [value, setValue] = useState(new Date());
+
+  return (
+    <div>
       <div className="flex justify-between">
         <div>
           <Dashboard></Dashboard>
@@ -15,18 +18,20 @@ class Home extends Component {
 
           <div className="w-[94%] mt-10">
             <div className="w-full flex justify-between">
-              <div className="h-56 w-1/3 bg-purple-700 ml-5 rounded-xl"></div>
-              <div className="h-56 w-2/3 bg-purple-900 rounded-xl"></div>
+              <div className="h-56 w-1/3  ml-5 rounded-xl shadow-[0_2px_18px_-10px_rgba(0,0,0,0.9)]">
+                <Calendar className="w-full h-full overflow-hidden" value={value} onChange={setValue} />
+              </div>
+              <div className="h-56 w-2/3 rounded-xl shadow-[0_2px_18px_-10px_rgba(0,0,0,0.9)]"></div>
             </div>
             <div className="w-full flex justify-between mt-5">
-              <div className="h-56 w-2/3 bg-slate-900 ml-5 rounded-xl"></div>
-              <div className="h-56 w-1/3 bg-yellow-700 rounded-xl"></div>
+              <div className="h-60 w-2/3 ml-5 rounded-xl shadow-[0_2px_18px_-10px_rgba(0,0,0,0.9)]"></div>
+              <div className="h-60 w-1/3 rounded-xl shadow-[0_2px_18px_-10px_rgba(0,0,0,0.9)]"></div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Home;
